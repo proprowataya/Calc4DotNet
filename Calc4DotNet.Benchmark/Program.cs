097518809edf445e741867e6fff831d7a6f8b921
Yuya Watari <8926297+proprowataya@users.noreply.github.com>
2020-04-26 16:17:23 +0900

    Add Benchmark project

---
+++ Calc4DotNet.Benchmark/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Calc4DotNet.Core;
+using Calc4DotNet.Core.Execution;
+using Calc4DotNet.Core.Operators;
+using Calc4DotNet.Core.Optimization;
+using Calc4DotNet.Core.SyntaxAnalysis;
+
+namespace Calc4DotNet.Benchmark
+{
+    using NumberType = Int32;
+
+    public class Program
+    {
+        public static string[] Sources => new string[]
+        {
+            "D[fib|n|n<=1?n?(n-1){fib}+(n-2){fib}] 38{fib}",
+            "D[tarai|x,y,z|x <= y ? y ? (((x - 1){tarai}y{tarai}z){tarai}((y - 1){tarai}z{tarai}x){tarai}((z - 1){tarai}x{tarai}y))] 18{tarai}12{tarai}5"
+        };
+
+        [ParamsSource(nameof(Sources))]
+        public string Source { get; set; }
+
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Program>();
+        }
+
+        [Benchmark]
+        public void RunByLowLevelExecutor()
+        {
+            // Compile
+            CompilationContext context = CompilationContext.Empty;
+            List<IToken> tokens = Lexer.Lex(Source, ref context);
+            IOperator op = Parser.Parse(tokens, ref context);
+            Optimizer.Optimize<NumberType>(ref op, ref context);
+            LowLevelModule<int> module = LowLevelCodeGenerator.Generate<NumberType>(op, context);
+
+            // Run
+            LowLevelExecutor.Execute(module);
+        }
+    }
+}
